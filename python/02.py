79eb9e8b503cec7315eeb3b318eaa9b0b1f44ddd
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-08-19 10:25:41 +0530

    02

--- python/02.py
+++ python/02.py
@@ -1,18 +1,23 @@
 name = input("enter a name")
 print(name,"is my name")
-
+#input
 number = input("enter a number")
 print(number,"is my number")
 
 #list
 mylist = ["apple", "banana", "cherry"]
 
+#loops
 for i in mylist:
     print(i)
 
 #function
 def my_function(a, b):
     return a+b
-
+# if-else
 hello = my_function(5, 10)
 print(hello)
+if hello > 10:
+    print("hello is greater than 10")
+else:
+    print("hello is less than or equal to 10")
