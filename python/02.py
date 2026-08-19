5d242c3f51b5a56f11182da597285e4a96d1495f
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-08-19 10:19:35 +0530

    02

---
+++ python/02.py
@@ -0,0 +1,18 @@
+name = input("enter a name")
+print(name,"is my name")
+
+number = input("enter a number")
+print(number,"is my number")
+
+#list
+mylist = ["apple", "banana", "cherry"]
+
+for i in mylist:
+    print(i)
+
+#function
+def my_function(a, b):
+    return a+b
+
+hello = my_function(5, 10)
+print(hello)
--- python/input.py
+++
@@ -1,8 +0,0 @@
-name = input("enter a name")
-print(name,"is my name")
-
-number = input("enter a number")
-print(number,"is my number")
-
-#list
-mylist = ["apple", "banana", "cherry"]
