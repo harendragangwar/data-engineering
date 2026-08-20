73cd7aab2748dcb2080d7660c70bccb924ff919d
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-08-21 00:21:28 +0530

    .

--- python/strings.py
+++ python/strings.py
@@ -2,4 +2,6 @@ x = "harendra gangwar"
 # we start counting from 0,   x[0], x[1], x[2], x[3].... x[n-1] and n is the length of the string 
 print(x[0])  # Output: h
 
-print(x[0:9]) # Output: harendra   0 to n-1
+print(x[0:8]) # Output: harendra   0 to n-1
+
+print(x[3:16]) # Output: endra gangwar 3 to n-1
