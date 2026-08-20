b9aea53cb2dc8153a1dc3957191ff4c462b89d01
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-08-21 00:07:55 +0530

    .

--- python/strings.py
+++ python/strings.py
@@ -2,4 +2,4 @@ x = "harendra gangwar"
 # we start counting from 0,   x[0], x[1], x[2], x[3].... x[n-1] and n is the total length of the string 
 print(x[0])  # Output: h
 
-print(x[0:9]) # Output: hare
+print(x[0:9]) # Output: harendra
