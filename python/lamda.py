34fa5cfa2fcfdbb69a9e879949d911b57a830ba0
harendra gangwar <106980501+harendragangwar@users.noreply.github.com>
2026-09-11 12:27:33 +0530

    lamda function

--- python/lamda.py
+++ python/lamda.py
@@ -5,3 +5,8 @@ def square(x):
 
 square = lambda x: x ** 2
 print(square(5))  # Output: 25
+
+numbers = [1, 2, 3, 4, 5, 6]
+# Keep only even numbers
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+print(even_numbers)  # Output: [2, 4, 6]
